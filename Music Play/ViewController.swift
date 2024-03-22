65a78216b6036f7898cc5ee43110399825c2bf83
ZverikRS <ZverikRS@autopole.ru>
2024-03-22 15:59:47 +0300

    Initial Commit

---
+++ Music Play/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Music Play
+//
+//  Created by ZverikRS on 22.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
